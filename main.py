c7f63fd5a891d5d12fa4d845c82612b9612c4ef3
Leonardo Melo <43916660+Ceu152@users.noreply.github.com>
2019-10-30 20:04:24 -0300

    Modificação da main
    Primeiros passos criados pela main.

--- main.py
+++ main.py
@@ -1,3 +1,49 @@
-#The main code goes here 
-#Start the graph interface
- 
+#Main program to create main frame
+
+# Creating Menu Bar example
+
+import tkinter as tk
+import tkinter.filedialog as fd
+import tkinter.messagebox as mb
+
+class App(tk.Tk):
+  def __init__(self):
+    super().__init__()
+    menu=tk.Menu(self)
+    self.title("Graph Work")
+    file_menu=tk.Menu(menu,tearoff=0)
+    file_menu.add_command(label="New file")
+    file_menu.add_command(label="Open", command = self.choose_file)
+    file_menu.add_separator()
+    file_menu.add_command(label="Save")
+    file_menu.add_command(label="Save as...")
+    menu.add_cascade(labe="File",menu=file_menu)
+    menu.add_command(label="About")
+    menu.add_command(label="Quit",command=self.leave_project) 
+    self.config(menu=menu)
+	
+  def choose_file(self):
+    filetypes=(("Plain text files","*.txt"),
+               ("Images","*.jpg *.gif *.png"),
+               ("All files", "*"))
+    filename=fd.askopenfilename(title="Open file",
+               initialdir="/",
+               filetypes=filetypes)
+    if filename:
+      print(filename)
+
+  def choose_directory(self):
+    directory=fd.askdirectory(title="Open directory",
+               initialdir="/")
+    if directory:
+      print(directory)
+  
+  #Criar uma janela que pede confirmação para sair.
+  def leave_project(self):
+    mb.askokcancel
+    print(response)
+    self.destroy
+		
+
+app=App()
+app.mainloop()
