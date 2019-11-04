b1efa0c2aa145d8266a280bf6da917a79fa216f5
Leonardo <leonardohenriquedemelo@Outlook.com>
2019-11-04 10:00:27 -0300

    Criação dos nós
    Criação de menus e nos no programa

--- main.py
+++ main.py
@@ -1,35 +1,35 @@
 #Main program to create main frame
 
-# Creating Menu Bar example
 
+#Area de imports do programa
 import tkinter as tk
 import tkinter.filedialog as fd
 import tkinter.messagebox as mb
+#-------***********-----------#
 from mainCanvas import makeCanvas as mc
+
 
 class App(tk.Tk):
   def __init__(self):
     super().__init__()
     menu=tk.Menu(self)
     self.title("Graph Work")
-    screen_width = self.winfo_screenwidth()
-    screen_height = self.winfo_screenheight()
-    self.geometry(str(screen_width)+"x"+str(screen_height))
+    self.screen_width = self.winfo_screenwidth()
+    self.screen_height = self.winfo_screenheight()
+    self.geometry(str(self.screen_width)+"x"+str(self.screen_height))
     file_menu=tk.Menu(menu,tearoff=0)
-    file_menu.add_command(label="New file")
+    file_menu.add_command(label="New file", command = self.new_canvas)
     file_menu.add_command(label="Open", command = self.choose_file)
     file_menu.add_separator()
-    file_menu.add_command(label="Save")
-    file_menu.add_command(label="Save as...")
+    file_menu.add_command(label="Save", command = self.save_file)
+    file_menu.add_command(label="Save as...", command = self.save_file)
     menu.add_cascade(labe="File",menu=file_menu)
     menu.add_command(label="About", command = self.create_aboutWindow)
-    menu.add_command(label="Quit",command=self.leave_project) 
+    menu.add_command(label="Quit",command=self.leave_project)
+    menu.add_command(label="Properties")
     self.config(menu=menu)
     
-    #Criação do canvas no projeto 
-    self.canvas = tk.Canvas(self, width = self.winfo_width(), height = self.winfo_height())
-    self.canvasOrg = mc(self.canvas)
-    
+  #Função para abrir um arquivo  
   def choose_file(self):
     filetypes=(("Plain text files","*.txt"),
                ("Images","*.jpg *.gif *.png"),
@@ -40,6 +40,7 @@ class App(tk.Tk):
     if filename:
       print(filename)
 
+  #Função para escolher um diretório 
   def choose_directory(self):
     directory=fd.askdirectory(title="Open directory",
                initialdir="/")
@@ -55,10 +56,27 @@ class App(tk.Tk):
     else:
       tk.messagebox.showinfo('Return', 'You will return to the application')
   
+  #Criação da Janela de about do programa
   def create_aboutWindow(self):
     aboutWindow = tk.Toplevel(self)
     display = tk.Label(aboutWindow, text = "Python work About Graphs\nGroup= Alex Junior Pereira\nLeonardo Henrique de Melo")
     display.pack()
-    
+
+  #Função para salvar um arquivo feito pelo programa
+  def save_file(self):
+    new_file=fd.asksaveasfile(title="Save file",
+                defaultextension=".txt",
+                filetypes=(("Text files","*.txt"),))
+    if new_file:
+      new_file.write()
+      new_file.close()
+
+  #Função para criar um novo canvas e colocá-lo na tela
+  def new_canvas(self):
+    #Criação do canvas no projeto 
+    self.canvas = tk.Canvas(self, bg = "white", width = self.screen_width, height = self.screen_height)
+    self.canvasOrg = mc(self, self.canvas)
+
+  
 app=App()
 app.mainloop()
--- mainCanvas.py
+++ mainCanvas.py
@@ -1,8 +1,48 @@
-# Writing text on a canvas example
+#Arquivo que manipula as ações sobre o canvas
+
 
 import tkinter as tk
+from functools import partial
 
 class makeCanvas:
-  def __init__(self, canvas):
+  #Variveis que pertencem a todos os nós
+  node_list = list()
+  node_number = 0
+  size = 30
+  def __init__(self, root ,canvas):
+    self.root = root
     self.canvas = canvas
     self.canvas.pack()
+    self.canvas.bind("<Button-1>", self.draw_item)
+
+  def draw_item(self, event):
+    x, y = event.x, event.y
+    bbox = (x-self.size, y-self.size, x+self.size, y+self.size)
+    self.canvas.create_oval(*bbox, fill="blue", activefill="grey")
+    elem_value = self.insert_node()
+    self.canvas.create_text(x,y,fill = "white", font = "Times 20 bold", text=str(elem_value))
+
+  def set_size(self, valor): 
+    self.size = valor
+
+  def insert_node(self):
+      i = 0
+      if(self.node_number > 0):
+        elem = self.node_list[i]
+        print(self.node_number)
+        while (elem != -1 and i < self.node_number-1):
+          elem = self.node_list[i]
+          i += 1
+        
+        i+= 1
+
+        if i < self.node_number-1:
+          self.node_list[i] = i
+        else:
+          self.node_list.append(i)
+          self.node_number += 1
+      else:
+          self.node_list.append(i)
+          self.node_number += 1
+
+      return i
